ec31f4627142b273d625c596899ff2b233578cf8
LuisRivero021298 <luisda021298@gmail.com>
2020-09-28 14:01:47 -0500

    change styles in card component

--- src/components/card.js
+++ src/components/card.js
@@ -1,25 +1,31 @@
 const cardTemplate = (characters) => {
-  if (status === 'unknown') status = 'No registered';
-  if (origin.name === 'unknown') origin.name = 'No registered';
+  if (status === "unknown") status = "No registered";
+  if (origin.name === "unknown") origin.name = "No registered";
 
-  return `${characters.map( character => 
-    `<article class="card" >
+  return `${characters
+    .map(
+      (character) =>
+        `<article class="card" >
       <a href="#/${character.id}">
         <div class="card__top">
           <img class="card__image" src="${character.image}" alt="card-image">
         </div>
         <div class="card__bottom spaced">
           <div>
-            <h2 class="card__title">${character.name}</h2>
-            <span class="card__label">${character.origin.name}</span>
+            <h3 class="card__title">${character.name}</h3>
           </div>
           <div>
             <span class="card__text">${character.species}</span>
-            <span class="card__label--${character.status.toLowerCase()}">${character.status}</span>
+            <span class="card__label--${character.status.toLowerCase()}">${
+          character.status
+        }</span>
           </div>
         </div>
       </a>
-    </article>`).join('')}
+    </article>`
+    )
+    .join("")}
   `;
-}
+};
 export default cardTemplate;
+
